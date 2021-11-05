6cbc02ed8c6a513f6f38e788fbaf0ff0f1e6eeb4
xiangshitong <572234205@qq.com>
2021-11-05 16:50:43 +0800

    master push-test commit2

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("我的git呢？");
         System.out.println("caocaocao");
+        System.out.println("my test2");
     }
 }
