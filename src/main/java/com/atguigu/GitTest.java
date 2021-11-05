437ce58dc666f66a36b70a01961fcf39599e46ad
xiangshitong <572234205@qq.com>
2021-11-05 16:37:05 +0800

    master commit3

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -2,11 +2,6 @@ package com.atguigu;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git!");
-        System.out.println("hello git2!");
-        System.out.println("hello git3!");
-        System.out.println("hot-fix");
-        System.out.println("master冲突");
-        System.out.println("hot-冲突");
+        System.out.println("我的git呢？");
     }
 }
